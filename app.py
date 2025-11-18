262492b68a75d27599dc3ad411968b4d3b65f8d0
git config --global user.name <s310424@student.cysh.cy.edu.tw>
2025-11-18 10:58:46 +0800

    fix template folder structure

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
-
-@app.route("/")
-def base():
-    return render_template("base.html")
 
 @app.route("/")
 def index():
@@ -19,5 +14,5 @@ def about():
 def contact():
     return render_template("contact.html")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
